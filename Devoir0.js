ef7f66df86a10cf25f404fb9dd3fa5a026858cd5
Caroll-Ann Blachette <Caroll-Ann.blanchette@outlook.com>
2019-01-11 16:29:40 -0500

    Update Devoir0.js

---
+++ Devoir0.js
@@ -0,0 +1,15 @@
+//Caroll-Ann Blanchette - 20002760
+//Alexandre Caron 
+
+var prenom = "Caroll Ann";
+var nom = "Blanchette Bisson"; 
+
+var salutation = "Bonjour, mon nom est" + " " + prenom + " " + nom + "!"; 
+
+var age = 27; 
+var discipline = "sciences biomédicales option sciences du vieillisement";
+var informations = "J'ai" + " " + age + " " + "ans et j'étudie en" + " " + discipline + "!";
+
+var introduction = salutation + "\n" + informations ;
+
+print(introduction);
